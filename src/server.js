b4cad5727d89d785cc439e6e847e865ce706a98b
Mykyta Petrakovskiy <nekitpetr46228@gmail.com>
2025-08-02 11:29:57 +0200

    finisz

---
+++ src/server.js
@@ -0,0 +1,81 @@
+import express from 'express';
+import pino from 'pino-http';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import { getEnvVar } from './utils/getEnvVar.js';
+import { getAllContacts, getContactById } from './services/contacts.js';
+
+dotenv.config();
+
+const PORT = Number(getEnvVar('PORT', '3000'));
+
+export const startServer = () => {
+  const app = express();
+
+  app.use(express.json());
+  app.use(cors());
+
+  app.use(
+    pino({
+      transport: {
+        target: 'pino-pretty',
+      },
+    }),
+  );
+
+  app.get('/', (req, res) => {
+    res.json({
+      message: 'Hello World!',
+    });
+  });
+
+  app.get('/contacts', async (req, res, next) => {
+  try {
+    const contacts = await getAllContacts();
+    res.status(200).json({
+      status: 200,
+      message: 'Successfully found contacts!',
+      data: contacts,
+    });
+  } catch (err) {
+    next(err);
+  }
+});
+
+app.get('/contacts/:contactId', async (req, res, next) => {
+  try {
+    const { contactId } = req.params;
+    const contact = await getContactById(contactId);
+
+    if (!contact) {
+      return res.status(404).json({ message: 'сontact not found' });
+    }
+
+    res.status(200).json({
+      status: 200,
+      message: `contact with id ${contactId}!`,
+      data: contact,
+    });
+  } catch (err) {
+    next(err);
+  }
+});
+
+
+  app.use('/', (req, res) => {
+    res.status(404).json({
+      message: 'Not found',
+    });
+  });
+
+  app.use((err, req, res, next) => {
+    res.status(500).json({
+      message: 'Something went wrong',
+      error: err.message,
+    });
+  });
+
+  app.listen(PORT, () => {
+    console.log(`port ${PORT}`);
+  });
+};
